183a077f478113db144b8e29b49b8901768ef379
Giacomo Chiarot <gicochiarot@gmail.com>
2024-06-03 18:16:26 +0200

    perf: Remove unnecessary imports and code

--- contracts/PoolsProxy.sol
+++ contracts/PoolsProxy.sol
@@ -1,25 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.21;
 
-import "./libraries/Errors.sol";
-import "./modules/BankWrapper.sol";
 import "./modules/RolesModifier.sol";
-import "./helpers/IERC20.sol";
-import "./interfaces/IBank.sol";
 
-struct Pool {
-    uint stakingStartedAt;
-    uint allocated;
-    bool canceled;
-    uint stakingPeriod;
-}
-
-contract PoolsProxy is BankWrapper, RolesModifier {
-    uint256 public numberOfPools;
-
-    mapping(uint256=>Pool) private idToPool;
-    mapping(address=>mapping(uint256=>uint256)) private addressToPoolStaking;
-    mapping(uint256=>uint256) private poolToStaked;
+contract PoolsProxy is RolesModifier {
     address public delegate;
 
     /*
@@ -27,8 +11,7 @@ contract PoolsProxy is BankWrapper, RolesModifier {
         @param _tokenContract The erc20 token address to be staked and unstaked.
         @param _implementation The address of the Pool's logic contract.
     */
-    constructor(address _rolesContract, address _bankContract, address _delegate)
-        BankWrapper(_bankContract)
+    constructor(address _rolesContract, address _delegate)
         RolesModifier(_rolesContract)
     {
         delegate = _delegate;

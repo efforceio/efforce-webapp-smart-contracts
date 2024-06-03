1447b6e2d7ecd687f4b0ef6cc35b57a84aa45f65
Giacomo Chiarot <gicochiarot@gmail.com>
2024-06-03 17:52:52 +0200

    refactor: Update implementation name to delegate

--- contracts/PoolsProxy.sol
+++ contracts/PoolsProxy.sol
@@ -20,18 +20,18 @@ contract PoolsProxy is BankWrapper, RolesModifier {
     mapping(uint256=>Pool) private idToPool;
     mapping(address=>mapping(uint256=>uint256)) private addressToPoolStaking;
     mapping(uint256=>uint256) private poolToStaked;
-    address public implementation;
+    address public delegate;
 
     /*
         @param _rolesContract The address of the roles smart contract.
         @param _tokenContract The erc20 token address to be staked and unstaked.
         @param _implementation The address of the Pool's logic contract.
     */
-    constructor(address _rolesContract, address _bankContract, address _implementation)
+    constructor(address _rolesContract, address _bankContract, address _delegate)
         BankWrapper(_bankContract)
         RolesModifier(_rolesContract)
     {
-        implementation = _implementation;
+        delegate = _delegate;
     }
 
     /*
@@ -39,11 +39,11 @@ contract PoolsProxy is BankWrapper, RolesModifier {
         @dev Can be called only by admins or contract owners.
         @param _implementation The address of the new Pool's logic contract.
     */
-    function setImplementation(address _implementation)
+    function setDelegate(address _delegate)
         external
         adminOrOwner(msg.sender)
     {
-        implementation = _implementation;
+        delegate = _delegate;
     }
 
     /*
@@ -51,13 +51,12 @@ contract PoolsProxy is BankWrapper, RolesModifier {
         This function will return whatever the implementation call returns.
     */
     function () payable public {
-        address _impl = implementation();
-        require(_impl != address(0));
+        require(delegate != address(0));
 
         assembly {
             let ptr := mload(0x40)
             calldatacopy(ptr, 0, calldatasize)
-            let result := delegatecall(gas, _impl, ptr, calldatasize, 0, 0)
+            let result := delegatecall(gas, delegate, ptr, calldatasize, 0, 0)
             let size := returndatasize
             returndatacopy(ptr, 0, size)
 

eb5b5e41b24ed74eef7f4a7148336baa712d7e1c
Giacomo Chiarot <gicochiarot@gmail.com>
2024-06-03 18:17:00 +0200

    fix: rename fallback function, add function calls, and fix local var reference

--- contracts/PoolsProxy.sol
+++ contracts/PoolsProxy.sol
@@ -33,14 +33,14 @@ contract PoolsProxy is RolesModifier {
       @dev Fallback function allowing to perform a delegatecall to the given implementation.
         This function will return whatever the implementation call returns.
     */
-    function () payable public {
+    fallback () external {
         require(delegate != address(0));
 
         assembly {
             let ptr := mload(0x40)
-            calldatacopy(ptr, 0, calldatasize)
-            let result := delegatecall(gas, delegate, ptr, calldatasize, 0, 0)
-            let size := returndatasize
+            calldatacopy(ptr, 0, calldatasize())
+            let result := delegatecall(gas(), sload(delegate.slot), ptr, calldatasize(), 0, 0)
+            let size := returndatasize()
             returndatacopy(ptr, 0, size)
 
             switch result

459f360b3876ef6d3344dca969cf015996e82258
Giacomo Chiarot <gicochiarot@gmail.com>
2024-06-19 12:41:47 +0200

    fix: replace comma with, operator and remove call to constant string, and add withdraw function to IBank

--- contracts/interfaces/IBank.sol
+++ contracts/interfaces/IBank.sol
@@ -9,5 +9,6 @@ interface IBank {
     function tokenAddress() external view returns(address);
 
     function withdraw(address recipient, uint amount) external;
+    function withdraw(address recipient, uint amount, address tokenAddress) external;
 
 }
